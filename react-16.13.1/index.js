35de5f326798292a7a4aa496da1e7d07b2871b3f
robert <>
2021-09-30 15:31:12 +0800

    debug note

--- react-16.13.1/index.js
+++ react-16.13.1/index.js
@@ -1,10 +1,50 @@
 function App() {
     const [count, setCount] = React.useState(0)
-    return <div>
-        <div>Hello, React Source Code!</div>
-        <div>Number: {count}</div>
-        <button onClick={() => setCount(v => v + 1)}>Increment</button>
-    </div>
+    // const [num, setNum] = React.useState(100)
+    // const [test, setTest] = useTest('aa');
+    return (
+        <div>
+            <h1>
+                {count}
+                <button onClick={() => {
+                    setCount(v => v + 1);
+                    setCount(v => v + 1)
+                    }}> + </button>
+            </h1>
+            <ComponentFunction />
+            <ComponentClass />
+            
+        </div>)
 }
+
+function ComponentFunction() {
+    console.log('come in ComponentFunction...');
+    
+    return <div><h2>ComponentFunction</h2></div>
+}
+class ComponentClass extends React.Component {
+    
+    componentDidMount(){
+        console.log("DidMount..");
+    }
+
+    componentDidUpdate(){
+        console.log('DidUpdate..');
+    }
+
+    render(){
+        return <div><h1>ComponentClass</h1></div>
+    }
+}
+
+// function useTest(initialState) {
+//     const [tmp,setTmp] = React.useState(initialState);
+
+//     const updateTmp = (value) => {
+//         console.log(value);
+//         setTmp(value);
+//     }
+//     return [tmp, updateTmp];
+// }
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
